a638ccaa97c56b188d9ddc1542921ab7ed8a4641
prathameshdabekar1 <prathameshdabekar1@gmail.com>
2025-02-26 15:02:18 +0530

    Add files via upload

---
+++ config.sh
@@ -0,0 +1,16 @@
+# Configuration for User Creation and Setup Script
+
+# User details
+USERNAME="arcgisservice"  # Replace with desired username
+
+# Packages to install
+PACKAGES=("nano" "wget" "tar" "tree" "unzip" "gettext-base" "curl" "lynx")
+
+# Data disk directory
+DATA_DISK_DIR="/mnt/data-disknew"
+
+# Cinc-client version
+CINC_VERSION="18.4.2"
+
+# ArcGIS cookbooks version
+ARCGIS_COOKBOOK_VERSION="5.1.0"
